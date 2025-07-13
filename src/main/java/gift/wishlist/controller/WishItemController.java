ebf0b860503ee5d44c8627c723f5f229fd68f243
lwj0831 <wjl0831@gmail.com>
2025-07-13 16:23:45 +0900

    chore: RESTful api형식에 맞게 url경로 수정

--- src/main/java/gift/wishlist/controller/WishItemController.java
+++ src/main/java/gift/wishlist/controller/WishItemController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/wishItems")
+@RequestMapping("/api/wish-items")
 public class WishItemController {
 
   private final WishItemService wishItemService;
